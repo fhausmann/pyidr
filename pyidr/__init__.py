25668577d6de5b1ec390e09510ddf61fa240851f
Fabian Hausmann <fabian.hausmann@zmnh.uni-hamburg.de>
2020-05-10 18:07:04 +0200

    Remove old function-like API

--- pyidr/__init__.py
+++ pyidr/__init__.py
@@ -1,5 +1,4 @@
 """pyIDR package."""
 __version__ = '0.1.0'
-from pyidr._idr import get_correspondence, idr
 from pyidr._base import IDR, CorrespondenceProfile
 __all__ = ["IDR", "CorrespondenceProfile"]
